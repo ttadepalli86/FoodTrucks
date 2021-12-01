34d52b64e29885debf9c4d99097e1ff4d40c123a
ttadepalli86 <tarun.jnitinc@gmail.com>
2021-12-01 16:47:33 +0000

    final Commit

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # build the flask container
-docker build -t prakhar1989/foodtrucks-web .
+docker build -t ttadepalli86/foodtrucks .
 
 # create the network
 docker network create foodtrucks-net
 
 # start the ES container
-docker run -d --name es --net foodtrucks-net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+docker run -d --name es --net foodtrucks-net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "ES_JAVA_OPTS=-Xmx1g -Xms1g" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+
+#Sleep And Wait for 5 Secs
+sleep 25
 
 # start the flask app container
-docker run -d --net foodtrucks-net -p 5000:5000 --name foodtrucks-web prakhar1989/foodtrucks-web
+docker run -d --net foodtrucks-net -p 5000:5000 --name ft ttadepalli86/foodtrucks
